83e1ab8913d5b269fcb749889f556738ee845de9
LucasLombardo <lucasalombardo@gmail.com>
2019-03-10 16:35:51 -0400

    Add eslint and lints project clean
    Add eslint and lints .js files clean with new settings
    - add eslint and configure to extend airbnb
    - add a few additional linting settings (indent 4, comma dangle allowed,
    favor backticks, no semi)
    - add vscode editor settings

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,10 @@
 import React from "react"
 
 const NotFoundPage = () => (
-  <div>
-    <h1>NOT FOUND</h1>
-    <p>404: Page not found.</p>
-  </div>
+    <div>
+        <h1>NOT FOUND</h1>
+        <p>404: Page not found.</p>
+    </div>
 )
 
 export default NotFoundPage
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,9 @@
 import React from "react"
 
 const IndexPage = () => (
-  <div>
-    <h1>Quoteworthy</h1>
-  </div>
+    <div>
+        <h1>Quoteworthy</h1>
+    </div>
 )
 
 export default IndexPage
