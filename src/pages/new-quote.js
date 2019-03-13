70dc3a39466df6c6d8f30bef8714e31f83fbd6b8
LucasLombardo <lucasalombardo@gmail.com>
2019-03-13 18:06:47 -0400

    Refactor newQuote moving logic into page
    Refactor and delete newQuote component, moving its logic into page

--- src/pages/new-quote.js
+++ src/pages/new-quote.js
@@ -1,11 +1,56 @@
-import React from "react"
+import React, { useContext, useState } from "react"
+import { navigate } from "gatsby"
+import UserContext from '../context/UserContext'
+import { createQuote } from "./api/client"
 import Layout from "../components/layout"
-import NewQuote from "../components/newQuote"
+import Form from "../styles/form"
 
-const NewQuotePage = () => (
-    <Layout>
-        <NewQuote />
-    </Layout>
-)
+const NewQuotePage = () => {
+    const [body, setBody] = useState(``)
+    const [attribution, setAttribution] = useState(``)
+
+    const user = useContext(UserContext).user || { id: ``, email: ``, token: `` }
+    const { invokeMessage } = useContext(UserContext)
+
+    const onNewQuote = (e) => {
+        e.preventDefault()
+        createQuote(body, attribution, user.token)
+            .then(() => setTimeout(() => {
+                navigate(`/`)
+                invokeMessage(`Successfully created quote`)
+            }, 200))
+            .catch(() => {
+                navigate(`/`)
+                invokeMessage(`Error creating quote, please try again.`, `failure`)
+            })
+    }
+
+    return (
+        <Layout>
+            <Form onSubmit={onNewQuote}>
+                <div>
+                    <h1>Create Quote</h1>
+                    <textarea
+                        value={body}
+                        onChange={e => setBody(e.target.value)}
+                        placeholder="quote body"
+                        type="text"
+                        name="body"
+                        required
+                    />
+                    <input
+                        value={attribution}
+                        onChange={e => setAttribution(e.target.value)}
+                        placeholder="attribution"
+                        type="text"
+                        name="attribution"
+                        required
+                    />
+                    <input type="submit" value="create quote" />
+                </div>
+            </Form>
+        </Layout>
+    )
+}
 
 export default NewQuotePage
