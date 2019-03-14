7073965fcbe2f7fc0b17254fb3c10c477714f49e
LucasLombardo <lucasalombardo@gmail.com>
2019-03-13 21:19:10 -0400

    Add loading icon on index page after 500ms
    Add a loading bar on the index page that starts after 500ms of loading
    without a response. Should help with letting user know its loading when
    heroku is booting up.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react"
 import styled from "styled-components"
+import ReactLoading from 'react-loading'
 import Layout from "../components/layout"
 import { readQuotes } from "../components/api/client"
 import Quote from "../components/quote"
@@ -7,15 +8,24 @@ import Quote from "../components/quote"
 const QuotesWrapper = styled.div`
     padding-top: 2em;
     text-align: center;
+    .loader {
+        position: fixed;
+        left: calc(50% - 20px);
+        top: 10%;
+        width: 40px;
+    }
 `
 
 const IndexPage = () => {
     const [quotes, setQuotes] = useState([])
+    const [loading, setLoading] = useState(false)
 
     useEffect(() => {
+        setLoading(true)
         readQuotes()
             .then((res) => {
                 setQuotes(res.data.quotes.reverse())
+                setLoading(false)
             })
     }, [])
 
@@ -26,6 +36,7 @@ const IndexPage = () => {
     return (
         <Layout>
             <QuotesWrapper>
+                {loading && <ReactLoading className="loader" delay="500" type="spin" color="#333" height="40px" width="40px" />}
                 <h1>All Quotes</h1>
                 {quotes.map(quote => (
                     <Quote key={quote.id} quoteData={quote} unmountQuote={unmountQuote} />
